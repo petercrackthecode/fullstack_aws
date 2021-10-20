0722d3a668563dd4f988f3dd9777f72e10dd5b89
petercrackthecode <nguyenvandung02051999@gmail.com>
2021-10-19 21:47:02 -0700

    Playing around with aws API Gateway, aws amplify, and aws lambda stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { API, Auth } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const callAPI = async () => {
+		const user = await Auth.currentAuthenticatedUser();
+		const token = user.signInUserSession.idToken.jwtToken;
+
+		console.log({token});
+
+		const requestInfo = {
+			headers: {
+				Authorization: token
+			}
+		}
+
+		const data = await API.get('fullstackAwsRestAPI', '/hello', requestInfo)
+		console.log(data)
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<button onClick={() => callAPI()}>Call API</button>
+			</header>
+		</div>
+	);
 }
 
-export default App;
+export default withAuthenticator(App);
